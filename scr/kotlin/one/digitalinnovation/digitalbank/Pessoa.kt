caf50c4e85000abb6e8b98f37aac5f78580974dc
ClRocco <igrocco2@gmail.com>
2021-03-04 16:25:22 -0300

    Adicionado private set a variável cpf.

--- scr/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
+++ scr/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
@@ -3,16 +3,14 @@ package one.digitalinnovation.digitalbank
 class Pessoa {
     var nome = "Claudio"
     var cpf: String = "808.946.157-34"
+    private set // Só permite a a variável acima só pode ser alterado dentro da própria classe.
 
-    inner class Endereco{
-        var rua: String = "Rua Dom Antônio de Macedo"
-    }
 }
 fun main(){
     val jether = Pessoa()
+    //jether.nome = "852" -- ) private set não permite escrever nesta variável
     println(jether.nome)
     println(jether.cpf)
 
-    println(jether.Endereco().rua)
 }
 
