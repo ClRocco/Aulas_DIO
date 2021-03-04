32afec07cec0ec95a9deb6d26d75d5b4f6ea5124
ClRocco <igrocco2@gmail.com>
2021-03-04 16:18:26 -0300

    Criação da inner class

--- scr/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
+++ scr/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
@@ -3,9 +3,16 @@ package one.digitalinnovation.digitalbank
 class Pessoa {
     var nome = "Claudio"
     var cpf: String = "808.946.157-34"
+
+    inner class Endereco{
+        var rua: String = "Rua Dom Antônio de Macedo"
+    }
 }
 fun main(){
     val jether = Pessoa()
     println(jether.nome)
     println(jether.cpf)
+
+    println(jether.Endereco().rua)
 }
+
