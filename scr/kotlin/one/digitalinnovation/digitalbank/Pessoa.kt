fa21cb9cbad2974cc7b483b20ccc8524f4089ec5
ClRocco <igrocco2@gmail.com>
2021-03-04 16:46:34 -0300

    Adicionado uma função a classe Pessoa.

--- scr/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
+++ scr/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
@@ -5,12 +5,18 @@ class Pessoa {
     var cpf: String = "808.946.157-34"
     private set // Só permite a a variável acima só pode ser alterado dentro da própria classe.
 
+    constructor()   // Construtor secundário
+
+    fun PessoaInfo() = " Nome: $nome \n CPF: $cpf"
 }
 fun main(){
     val jether = Pessoa()
-    //jether.nome = "852" -- ) private set não permite escrever nesta variável
-    println(jether.nome)
-    println(jether.cpf)
 
+    //jether.nome = "852" -- ) private set não permite escrever nesta variável
+
+    //println(jether.nome)
+    //println(jether.cpf)
+
+    println(jether.PessoaInfo())
 }
 
